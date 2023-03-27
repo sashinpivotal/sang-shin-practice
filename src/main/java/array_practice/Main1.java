2b4f06d424edde705c2e1ea4fd450dbb1567c203
Sang Shin <sangshinpassion@gmail.com>
2023-03-27 15:15:22 -0400

    assigment #1 done

---
+++ src/main/java/array_practice/Main1.java
@@ -0,0 +1,16 @@
+package array_practice;
+
+public class Main1 {
+    public static void main(String[] args) {
+
+        int[] myIntArray = {1, 2, 3};
+
+        for (int i = 0; i < myIntArray.length ; i++) {
+            System.out.println(myIntArray[i]);
+        }
+
+        for (int myInt : myIntArray) {
+            System.out.println(myInt);
+        }
+    }
+}
